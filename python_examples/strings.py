a8e24f2080d509dd5fa94c53999f029ccba7cb12
Fabrice Devaux <fabrice.devaux@gmail.com>
2025-10-18 08:01:12 +0200

    Add string example

--- python_examples/strings.py
+++ python_examples/strings.py
@@ -1,1 +1,7 @@
 """Voorbeelden: werken met strings."""
+
+# String "slicen": Met string_var[x:y] krijg je het deel van de string
+# van karakter x t.e.m. karakter y. Het eerste karakter is 0!
+string_var = "Welkom bij Syntra"
+print(string_var[3:6])
+# output: kom
